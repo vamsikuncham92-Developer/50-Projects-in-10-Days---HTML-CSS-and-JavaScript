d41435ace80527b342b8f55aef1ae32769121e42
vamsikuncham92@gmail.com <vamsikuncham92@gmail.com>
2026-01-22 02:56:00 +0530

    clock-theme

--- Starter-Project/script.js
+++ Starter-Project/script.js
@@ -1,1 +1,12 @@
 
+// const hummus = function(factor){
+//     const ingredient = function(amount,unit,name){
+//       let ingredientAmount = amount * factor;
+//       if(ingredientAmount > 1){
+//         unit += "s";
+//       }
+//       console.log(`${ingredientAmount}${unit}${name}`)
+//     }
+//     ingredient(1,'can','chickpeas');
+// }
+// hummus(0.2)
